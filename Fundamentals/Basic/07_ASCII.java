64f0ad9872b55b08f0361ea12a2f22c400e40f6a
Riya  Maity <maity.riya1906@gmail.com>
2026-03-02 22:22:54 +0530

    Add program to find ASCII value of a character

---
+++ Fundamentals/Basic/07_ASCII.java
@@ -0,0 +1,8 @@
+// Write a Program to Find ASCII Value of a Character
+class ASCII {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int value = (int)ch;
+        System.out.println("Ascii value of " + ch +" is " + value );
+    }
+}
