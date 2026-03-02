451edfca8f68ce3fdd9af1059416890ba5d35e2c
Riya  Maity <maity.riya1906@gmail.com>
2026-03-02 21:25:02 +0530

    Add division program to compute quotient and remainder
    Implemented a program to compute the quotient and remainder of two integers entered by the user.

--- Fundamentals/Basic/04_Division.java
+++ Fundamentals/Basic/04_Division.java
@@ -1,1 +1,18 @@
+// Write a program where the user is asked to enter two integers (divisor and dividend) and the quotient and the remainder of their division is computed.(Both divisor and dividend should be integers.)
 
+import java.util.Scanner;
+class Division 
+{
+    public static void main ( String []args)
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter divisor : ");
+        int divisor = sc.nextInt();
+        System.out.print("Enter dividend  : ");
+        int dividend = sc.nextInt();
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("Quotient is " + quotient);
+        System.out.println("Remainder is " + remainder);
+    }
+}
