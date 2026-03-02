ffcf3366dd7db9d95dffdb11ed786f806aff520b
Riya  Maity <maity.riya1906@gmail.com>
2026-03-02 23:22:42 +0530

    To check number is even or odd numbers

---
+++ Fundamentals/If-Else/01_EvenOdd.java
@@ -0,0 +1,18 @@
+// Write a Program to Check Whether Number is Even or Odd
+
+class EvenOdd
+{
+    public static void main(String []args)
+    {
+        int num = 10;
+        if(num % 2 == 0)
+        {
+            System.out.println("The number is even.");
+        }
+        
+        else
+        {
+            System.out.println("The number is odd.");
+        }
+    }
+}
