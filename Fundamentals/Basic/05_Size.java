fa5df30cd24b494e4b49f2e49c9186facef6a567
Riya  Maity <maity.riya1906@gmail.com>
2026-03-02 21:24:31 +0530

    Implement Size.java to display data type sizes
    The program outputs the sizes of various data types in bits, including int, float, double, and char.

---
+++ Fundamentals/Basic/05_Size.java
@@ -0,0 +1,9 @@
+//Write a Program to Find Size of int, float, double and char in your computer
+public class Size {
+    public static void main(String[] args) {
+        System.out.println("Size of int: " + Integer.SIZE + " bits");
+        System.out.println("Size of float: " + Float.SIZE + " bits");
+        System.out.println("Size of double: " + Double.SIZE + " bits");
+        System.out.println("Size of char: " + Character.SIZE + " bits");
+    }
+}
