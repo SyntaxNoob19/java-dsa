2e2bde0f148446751f02c17a1bafbabe830705df
Riya  Maity <maity.riya1906@gmail.com>
2026-03-08 01:23:57 +0530

    Add SolidRectangle.java to print star patterns

---
+++ Fundamentals/Pattern/01_SolidRectangle.java
@@ -0,0 +1,25 @@
+/*
+1) Solid Rectangular star
+* * * * *
+* * * * *
+* * * * *
+*/
+
+class Calculator {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 5;
+        
+        for (int i = 1 ; i<=n ; i++)
+        {
+            for (int j = 1 ; j<=m ; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+
