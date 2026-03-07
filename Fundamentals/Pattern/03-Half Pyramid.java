78e8f2f1f5349abe417224dee5b3110a7ac4beac
Riya  Maity <maity.riya1906@gmail.com>
2026-03-08 01:47:02 +0530

    Add Half Pyramid Star Pattern implementation

---
+++ Fundamentals/Pattern/03-Half Pyramid.java
@@ -0,0 +1,24 @@
+/*
+3) Half Pyramid Star Pattern
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
+
+class HalfPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        int i, j;
+        for ( i = 1 ; i<=n ; i++)
+        {
+            for (j = 1 ; j<=i ; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
