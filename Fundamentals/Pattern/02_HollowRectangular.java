a47397d46f3cef0a26659d258bb39bcdd248b00d
Riya  Maity <maity.riya1906@gmail.com>
2026-03-08 01:37:20 +0530

    Add HollowRectangular.java for star pattern

---
+++ Fundamentals/Pattern/02_HollowRectangular.java
@@ -0,0 +1,28 @@
+/*
+2) Hollow Rectangular star
+* * * * *
+*       *
+* * * * *
+*/
+
+class HollowRectangular {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 5;
+        int i, j;
+        for ( i = 1 ; i<=n ; i++)
+        {
+            for (j = 1 ; j<=m ; j++)
+            {
+                if( i==1 || i==n || j==1 || j==m)
+                System.out.print("* ");
+                
+                else
+                System.out.print("  ");
+            }
+            
+            System.out.println();
+        }
+        
+    }
+}
